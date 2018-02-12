2adf415f6ef7fffce4fad9eedd12b0d57ba9cd4f
Suikaba <suibaka@hotmail.co.jp>
2018-02-13 05:56:37 +0900

    fix dijkstra

--- src/graph/dijkstra.rs
+++ src/graph/dijkstra.rs
@@ -2,17 +2,18 @@
 fn dijkstra(g: &WeightedGraph, s: usize) -> (std::vec::Vec<Option<i32>>, std::vec::Vec<Option<usize>>) {
     let mut d = vec![None; g.node_size()];
     let mut prev = vec![None; g.node_size()];
-    let mut que: BinaryHeap<(i32, usize, usize)> = BinaryHeap::new();
+    let mut que: BinaryHeap<(i32, usize)> = BinaryHeap::new();
+    d[s] = Some(0);
     que.push((0, s));
-    while let Some((cur_d, v, pre)) = que.pop() {
-        if d[v].is_some() {
+    while let Some((cur_d, v)) = que.pop() {
+        if d[v].unwrap() < -cur_d {
             continue;
         }
-        d[v] = Some(-cur_d); // reverse order
-        prev[v] = Some(pre);
         for ref e in g.edges(v) {
-            if d[e.to].is_none() {
-                que.push((cur_d - e.weight, e.to, v));
+            if d[e.to].is_none() || d[e.to].unwrap() > -cur_d + e.weight {
+                d[e.to] = Some(-cur_d + e.weight);
+                prev[e.to] = Some(v);
+                que.push((cur_d - e.weight, e.to));
             }
         }
     }
